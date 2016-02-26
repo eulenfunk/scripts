be3eb3f15b67c80cde955ebf7b702964b7de1fd6
Dominik Fischer <dom_fischer@web.de>
2016-02-26 13:07:57 +0100

    documentation in gliffy

--- supernode/supernode-rc.sh
+++ supernode/supernode-rc.sh
@@ -8,13 +8,14 @@ BASE=/opt/eulenfunk/supernode
 IPV6_PREFIX_LENGTH=${SUPERNODE_IPV6_PREFIX##*/}
 IPV6_NET_ADDRESS=${SUPERNODE_IPV6_PREFIX%/*}
 SUPERNODE_IPV6_TRANS_REMOTE=${IPV6_NET_ADDRESS}1
+SUPERNODE_IPV6_CLIENT_PREFIX=${IPV6_NET_ADDRESS}/64
 
 ${DBG} /sbin/ip -4 route add table 42 default via 172.31.254.254
 ${DBG} /sbin/ip -6 route add table 42 ${SUPERNODE_IPV6_TRANS_REMOTE} dev eth1
 ${DBG} /sbin/ip -6 route add table 42 default via ${SUPERNODE_IPV6_TRANS_REMOTE} dev eth1
 
 ${DBG} /sbin/ip -4 route add table 42 ${SUPERNODE_IPV4_CLIENT_NET} dev br0 scope link
-${DBG} /sbin/ip -6 route add table 42 ${SUPERNODE_IPV6_PREFIX} dev br0
+${DBG} /sbin/ip -6 route add table 42 ${SUPERNODE_IPV6_CLIENT_PREFIX} dev br0
 
 ${DBG} /sbin/ip -4 rule add prio 1000 from ${SUPERNODE_IPV4_CLIENT_NET} lookup 42
 ${DBG} /sbin/ip -6 rule add prio 1000 from ${SUPERNODE_IPV6_PREFIX} lookup 42
