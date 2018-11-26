9c24e10a4facb118477247bfe5fc7ff71f009556
Adorfer <adorferen@gmail.com>
2018-11-26 12:29:12 +0100

    mehr logging

--- gwcheck.sh
+++ gwcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# notnagel-script: openwrt-router rebooten, wenn er den uplink nimmer findet
 upgrade_started='/tmp/autoupdate.lock'
-gateway=172.29.5.1
+gateway=...
+wanif=eth0.2
 
 [ -f $upgrade_started ] && exit
 
@@ -9,19 +9,34 @@ nogw=false
 ping -c 10 -W 1 $gateway &>/dev/nul|| nogw=true
 echo nogw equals "$nogw"
 if [ "$nogw" == "true" ] ; then
-  if [ -f /tmp/gw ] ; then # nur wenn er den uplinke überhaupt einmal gefunden hat. 
+  logger gwcheck "gw $gateway NOT pingable"
+  if [ -f /tmp/gw ] ; then
     if [ -f /tmp/gwgone.3 ] ; then
       [ -f $upgrade_started ] && exit
+      logger gwcheck "rebooting"
+      sleep 10
       reboot
     elif [ -f /tmp/gwgone.2 ] ; then
       touch /tmp/gwgone.3
+      logger gwcheck "restarting networking and firewall"
+      ifconfig wanif down
+      sleep 2
+      ifconfig wanif up
+      /etc/init.d/networking restart
+      sleep 2
+      /etc/init.d/firewall restart
     elif [ -f /tmp/gwgone.1 ] ; then
       touch /tmp/gwgone.2
+      logger gwcheck "ifconfig down and up"
+      ifconfig wanif down
+      sleep 3
+      ifconfig wanif up
     else
       touch /tmp/gwgone.1
     fi
   fi
 else
   touch /tmp/gw
+  logger gwcheck "gw $gateway pingable OK"
   rm -f /tmp/gwgone.* 2>/dev/null
 fi
