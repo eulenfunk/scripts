41784b2149641bffb9cf475b1ada12d8c9df56ef
Adorfer <adorferen@gmail.com>
2020-02-14 23:29:00 +0100

    vmbr nat setup bridge helperscript

---
+++ dnsmasqmini.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# mini dnsmasq dhcp for masquerading network
+
+natif=vmbr9 #interface where the clients reside
+rangestart=100
+rangeend=199
+
+if [ "$(cat /sys/class/net/$natif/carrier)" == "1" ] ; then
+  natnet=$(/sbin/ip -f inet a s $natif | awk '/inet/ { print $2 }'|head -1)
+  natip=$(/sbin/ip -f inet a s $natif | awk '/inet/ { print $2 }'|cut -d"/" -f1|head -1)
+  natbase=$(/sbin/ip -f inet a s $natif | awk '/inet/ { print $2 }'|cut -d"." -f1-3|head -1)
+  defaultif=$(/sbin/ip -f inet route | awk '/default/ { print $5 }'|head -1 )
+  defaultgw=$(/sbin/ip -f inet route | awk '/default/ { print $3 }'|head -1)
+  defaultnetip=$(/sbin/ip -f inet addr show $defaultif | awk '/inet/ { print $2 }'|cut -d"/" -f1|head -1)
+ cat > dnsmasqmini.conf << EOL
+interface=$natif
+server=/*/8.8.4.4
+server=/*/8.8.8.8
+dhcp-range=$natbase.$rangestart,$natbase.$rangeend,255.255.255.0,144h
+dhcp-option=3,$natip
+dhcp-option=6,$natip
+dhcp-authoritative
+no-negcache
+EOL
+ fi
+
+ kill $(ps ax |grep dnsmasqmini.conf|grep "\-C" |awk '{ print $1 }'|head -1) 2>/dev/nul
+ dnsmasq -C dnsmasqmini.conf
+
+
+
--- vmbrnat.sh
+++ vmbrnat.sh
@@ -4,10 +4,10 @@
 natif=vmbr9 #interface where the clients reside
 
 if [ "$(cat /sys/class/net/$natif/operstate)" == "up" ] ; then
-  natnet=$(/sbin/ip -f inet a s vmbr9 | awk '/inet/ { print $2 }')
-  defaultif=$(/sbin/ip -f inet route | awk '/default/ { print $5 }')
-  defaultgw=$(/sbin/ip -f inet route | awk '/default/ { print $3 }')
-  defaultnetip=$(/sbin/ip -f inet addr show $defaultif | awk '/inet/ { print $2 }'|cut -d"/" -f1)
+  natnet=$(/sbin/ip -f inet a s vmbr9 | awk '/inet/ { print $2 }'|head -1)
+  defaultif=$(/sbin/ip -f inet route | awk '/default/ { print $5 }'|head -1)
+  defaultgw=$(/sbin/ip -f inet route | awk '/default/ { print $3 }'|head -1)
+  defaultnetip=$(/sbin/ip -f inet addr show $defaultif | awk '/inet/ { print $2 }'|cut -d"/" -f1|head -1)
   echo "1" > /proc/sys/net/ipv4/ip_forward
   iptables -t nat -A POSTROUTING -o $defaultif -s $natnet -j SNAT --to $defaultnetip
   iptables -t nat -A POSTROUTING -o $defaultif -s $natnet -j MASQUERADE
